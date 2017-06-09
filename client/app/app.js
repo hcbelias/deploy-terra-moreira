d68a4ca9e1113fd760e02a3e15650c5cadf72b29
henrique elias <hcbe2004@gmail.com>
2017-06-08 22:02:43 -0300

    deploying contact

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 angular.module('webapp', ['webapp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ui.router', 'validation.match', 'ngMaterial', 'ngMessages', 'ngAnimate']).config(function ($urlRouterProvider, $locationProvider, $mdThemingProvider) {
-  $urlRouterProvider.otherwise('/');
-  $locationProvider.html5Mode(true);
-  $mdThemingProvider.theme('default').primaryPalette('grey').warnPalette('deep-orange').accentPalette('red').dark();
-
-  $mdThemingProvider.theme('docs-dark').primaryPalette('yellow').dark();
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
+    $mdThemingProvider.theme('default').primaryPalette('grey').warnPalette('deep-orange').accentPalette('red').dark();
 });
 //# sourceMappingURL=../app/app.js.map
 'use strict';
@@ -50,11 +48,37 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 angular.module('webapp').config(function ($stateProvider) {
   $stateProvider.state('main', {
-    url: '/?q',
+    name: 'main',
+    url: '/',
     template: '<main layout="column" flex class="home-page"></main>'
   });
 });
 //# sourceMappingURL=../../app/main/main.js.map
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+  var MessageController = function MessageController() {
+    _classCallCheck(this, MessageController);
+  };
+
+  angular.module('webapp').component('message', {
+    templateUrl: 'app/message/message.html',
+    controller: MessageController
+  });
+})();
+//# sourceMappingURL=../../app/message/message.controller.js.map
+'use strict';
+
+angular.module('webapp').config(function ($stateProvider) {
+  $stateProvider.state('message', {
+    name: 'message',
+    url: '/message',
+    template: '<message layout="column" flex class="home-page"></message>'
+  });
+});
+//# sourceMappingURL=../../app/message/message.js.map
 'use strict';
 
 (function () {
@@ -238,45 +262,7 @@ angular.module('webapp').config(function ($stateProvider) {
 
 (function () {
 
-  angular.module('webapp.auth').run(function ($rootScope, $state, Auth) {
-    // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
-    $rootScope.$on('$stateChangeStart', function (event, next) {
-      if (next.name === 'login') {
-        Auth.isLoggedIn(_.noop).then(function (is) {
-          if (is) {
-            event.preventDefault();
-            $state.go('main');
-          }
-        });
-      }
-
-      if (!next.authenticate) {
-        return;
-      }
-
-      if (typeof next.authenticate === 'string') {
-        Auth.hasRole(next.authenticate, _.noop).then(function (has) {
-          if (has) {
-            return;
-          }
-
-          event.preventDefault();
-          return Auth.isLoggedIn(_.noop).then(function (is) {
-            $state.go(is ? 'main' : 'login');
-          });
-        });
-      } else {
-        Auth.isLoggedIn(_.noop).then(function (is) {
-          if (is) {
-            return;
-          }
-
-          event.preventDefault();
-          $state.go('login');
-        });
-      }
-    });
-  });
+  angular.module('webapp.auth').run(function ($rootScope, $state, Auth) {});
 })();
 //# sourceMappingURL=../../components/auth/router.decorator.js.map
 'use strict';
@@ -343,9 +329,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }
 
     _createClass(FacebookButtonController, [{
-      key: "openFacebook",
+      key: 'openFacebook',
       value: function openFacebook() {
-        this.window.open("https://www.facebook.com/advterramoreira/", "_blank");
+        this.window.open('https://www.facebook.com/advterramoreira/', '_blank');
       }
     }]);
 
@@ -383,30 +369,40 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 (function () {
   var ToolbarController = function () {
-    function ToolbarController($mdSidenav, $stateParams) {
+    function ToolbarController($mdSidenav, $state) {
       _classCallCheck(this, ToolbarController);
 
       this.title = 'Terra Moreira';
       this.toggleLeft = this.buildToggler('left');
       this.toggleRight = this.buildToggler('right');
       this.navbar = this.getNavbar();
-
+      this.state = $state;
       this.image = 'assets/img/cartao.jpg';
     }
 
     _createClass(ToolbarController, [{
       key: 'getNavbar',
       value: function getNavbar() {
+        this.currentNavItem = 'Home';
         return [{
-          title: 'Home'
-        }, {
-          title: 'Áreas de Atuação'
-        }, {
-          title: 'Área do Cliente'
-        }, {
-          title: 'Dicionário Jurídico'
-        }, {
-          title: 'Contato'
+          title: 'Home',
+          state: 'main'
+        },
+        //{
+        //  title: 'Áreas de Atuação',
+        //  state: 'main'
+        //},
+        //{
+        //  title: 'Área do Cliente',
+        //  state: 'main'
+        //},
+        //{
+        //  title: 'Dicionário Jurídico',
+        //  state: 'main'
+        //},
+        {
+          title: 'Contato',
+          state: 'message'
         }];
       }
     }, {
@@ -415,6 +411,11 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         return function () {
           this.mdSidenav(componentId).toggle();
         };
+      }
+    }, {
+      key: 'redirectHomePage',
+      value: function redirectHomePage() {
+        this.state.go('main');
       }
     }]);
 
@@ -544,8 +545,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 })();
 //# sourceMappingURL=../../components/util/util.service.js.map
 angular.module("webapp").run(["$templateCache", function($templateCache) {$templateCache.put("app/main/main.html","<md-content><div class=\"image-background md-whiteframe-18dp\"><img ng-src=\"{{$ctrl.image}}\" alt=\"Advocacia Terra Moreira\" class=\"md-card-image img-responsive content md-whiteframe-20d\"/></div><md-toolbar class=\"content-divider\"><md-toolbar-tools></md-toolbar-tools></md-toolbar><div class=\"card-section md-whiteframe-16dp\"><md-card class=\"content card md-whiteframe-20dp content-card\"><md-toolbar><md-toolbar-tools><span class=\"md-headline\">Quem Somos</span></md-toolbar-tools></md-toolbar><div layout=\"row\" class=\"degrade\"></div><md-card-content><p class=\"md-body-1\">O escritório Terra Moreira Sociedade de Advogados está inscrito na OAB/MG sob o número 5.536, sendo um escritório completo que oferece apoio profissional experiente para indivíduos e empresas. Somos um escritório consolidado no mercado da advocacia, com atuação em diversas áreas do Direito.</p><p class=\"md-body-1\">O diferencial do escritório é a sua atuação personalizada e estreita relação com o cliente, oferecendo soluções rápidas, seguras e eficazes para os questionamentos apresentados, pautando-se na ética profissional e absoluto sigilo nas negociações, sempre visando o melhor interesse do cliente. </p></md-card-content></md-card><md-card class=\"content card md-whiteframe-20dp content-card\"><md-toolbar><md-toolbar-tools><span class=\"md-headline\">Valores e Missão</span></md-toolbar-tools></md-toolbar><div layout=\"row\" class=\"degrade\"></div><md-card-content><p class=\"md-body-1\">Procuramos construir uma banca de advocacia sólida, respeitada, moderna, eficiente e dinâmica, pautada em princípios de profissionalismo, lealdade, parceria, modernidade e pioneirismo.</p><p class=\"md-body-1\">Nosso maior foco é alcançar os resultados almejados por nossos clientes, sendo que o fruto de nosso sucesso deve-se ao planejamento, profissionais qualificados, trabalho árduo, entusiasmo e dedicação à causa.</p><p class=\"md-body-1\">Agradecemos imensamente a todos os clientes, amigos, parceiros e funcionários que de alguma forma nos ajudam a implementar nossos projetos.</p></md-card-content></md-card><md-card class=\"content card md-whiteframe-20dp content-card\"><md-toolbar><md-toolbar-tools><span class=\"md-headline\">Onde Estamos</span></md-toolbar-tools></md-toolbar><div layout=\"row\" class=\"degrade\"></div><md-card-content><p class=\"md-body-1\">Nosso escritório está localizado na rua Tenente de Brito Melo, 404, bairro Barro Preto, em Belo Horizonte-MG.</p><iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.0765208413886!2d-43.956666084579716!3d-19.92117998660866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa697436622eefd%3A0x8ad2377d21b0ef84!2sAdvocacia+Terra+Moreira!5e0!3m2!1sen!2sus!4v1496805646780\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" allowfullscreen=\"\"></iframe></md-card-content></md-card></div><md-toolbar class=\"content-divider\"><md-toolbar-tools></md-toolbar-tools></md-toolbar><contact class=\"contact\"></contact><md-toolbar class=\"content-divider\"><md-toolbar-tools></md-toolbar-tools></md-toolbar></md-content>");
+$templateCache.put("app/message/message.html","<contact class=\"message-container\"></contact>");
 $templateCache.put("components/contact/contact.html","<md-card class=\"content card md-whiteframe-20dp contact-card content-card\"><md-toolbar><md-toolbar-tools><span class=\"md-headline\">Fale Conosco</span></md-toolbar-tools></md-toolbar><div layout=\"row\" class=\"degrade\"></div><md-card-content layout-align=\"center center\" layout=\"row\"><form name=\"contactForm\" layout=\"column\" layout-align=\"start start\" ng-submit=\"$ctrl.sendEmail()\"><div layout=\"column\" layout-gt-sm=\"row\"><md-input-container><label for=\"clientName\">Nome</label><input ng-model=\"$ctrl.name\" required=\"required\" name=\"clientName\" md-no-asterisk=\"md-no-asterisk\"/><div ng-messages=\"contactForm.clientName.$error\"><div ng-message=\"required\">Preenchimento obrigatório</div></div></md-input-container><md-input-container><label>Telefone</label><input ng-model=\"$ctrl.phone\" type=\"phone\" name=\"clientPhone\" required=\"required\" md-no-asterisk=\"md-no-asterisk\"/><div ng-messages=\"contactForm.clientPhone.$error\"><div ng-message=\"required\">Preenchimento obrigatório</div></div></md-input-container><md-input-container><label>E-mail</label><input ng-model=\"$ctrl.email\" type=\"email\" name=\"clientEmail\"/><div ng-messages=\"contactForm.clientEmail.$error\"><div ng-message=\"pattern\">Preenchimento obrigatório</div></div></md-input-container><md-input-container><label>Descrição</label><textarea ng-model=\"$ctrl.description\" name=\"clientDescription\" md-maxlength=\"250\" maxlength=\"250\" minlength=\"5\" md-minlength=\"5\" required=\"required\" md-no-asterisk=\"md-no-asterisk\" md-select-on-focus=\"md-select-on-focus\"></textarea><div ng-messages=\"contactForm.clientDescription.$error\"><div ng-message=\"required\">Preenchimento obrigatório</div><div ng-message=\"minlength\">Por favor, escreva uma pequena descrição</div></div></md-input-container><md-input-container><md-button type=\"submit\" ng-disabled=\"contactForm.$invalid\" class=\"send-button\">Enviar</md-button></md-input-container></div></form></md-card-content><div layout=\"row\" class=\"degrade\"></div><facebook-button flex=\"flex\"></facebook-button></md-card>");
 $templateCache.put("components/contact-info/contact-info.html","<div layout-padding=\"layout-padding\" layout=\"row\" layout-align=\"center center\" class=\"contact-info\"><div class=\"contact-info-icon\"><md-button aria-label=\"Nos visite no Facebook\" class=\"md-fab contact-icon\"><md-icon class=\"material-icons contact-info-icon-text\">{{ $ctrl.icon }}</md-icon></md-button></div><div flex=\"80\" class=\"contact-info-phone-text\"><div layout=\"row\"> <span class=\"prefix\">(31) </span><span class=\"phone\">{{ $ctrl.phone1 }}</span></div><div layout=\"row\"> <span class=\"prefix\">(31) </span><span class=\"phone\">{{ $ctrl.phone2 }}</span></div><div layout=\"row\"> <span class=\"prefix\">(31) </span><span class=\"phone\">{{ $ctrl.phone3 }}</span></div></div></div>");
-$templateCache.put("components/facebook-button/facebook-button.html","<div layout-padding=\"layout-padding\" layout-margin=\"layout-margin\" layout-align=\"center center\" ng-click=\"$ctrl.openFacebook()\" layout=\"row\"><md-button aria-label=\"Nos visite no Facebook\" class=\"md-fab\"><img ng-src=\"/assets/img/facebook-white.png\" alt=\"Advocacia Terra Moreira\"/></md-button><span>Acesse nossa página no Facebook</span></div>");
-$templateCache.put("components/footer/footer.html","<div layout=\"column\" layout-gt-xs=\"row\" flex=\"50\" layout-align=\"center center\"><contact-info icon=\"phone\" phone1=\"2523-8533\" phone2=\"3018-6128\" phone3=\"3515-5099\"></contact-info><contact-info icon=\"smartphone\" phone1=\"98563-2664\" phone2=\"99376-3195\" phone3=\"98104-7997\"></contact-info></div><er layout-align=\"center center\" layout=\"column\" flex=\"50\"><span class=\"footer-text md-title\">Rua Tenente Brito de Melo, 404</span><span class=\"footer-text md-title\">Barro Preto - Belo Horizonte/MG</span><span class=\"footer-text md-title\">CEP: 30180-072</span></er>");
-$templateCache.put("components/toolbar/toolbar.html","<md-toolbar md-scroll-shrink=\"\" class=\"md-toolbar md-whiteframe-12dp\"><div layout=\"row\" layout-align=\"space-between center\" class=\"md-toolbar-tools content\"><img ng-src=\"{{$ctrl.image}}\" alt=\"Advocacia Terra Moreira\" class=\"md-card-image\"/><md-nav-bar flex=\"\" md-selected-nav-item=\"currentNavItem\" nav-bar-aria-label=\"navigation links\" hide=\"hide\" show-gt-sm=\"\" class=\"md-warn\"><md-nav-item ng-repeat=\"item in $ctrl.navbar\" md-nav-click=\"goto(\'page1\')\" name=\"page1\">{{item.title}}</md-nav-item></md-nav-bar><md-button><i class=\"inset material-icons\">menu</i></md-button></div><div layout=\"row\" class=\"degrade\"></div></md-toolbar>");}]);
+$templateCache.put("components/facebook-button/facebook-button.html","<div layout-padding=\"layout-padding\" layout-margin=\"layout-margin\" layout-align=\"center center\" ng-click=\"$ctrl.openFacebook()\" layout=\"column\" layout-gt-xs=\"row\"><md-button aria-label=\"Nos visite no Facebook\" class=\"md-fab\"><img ng-src=\"/assets/img/facebook-white.png\" alt=\"Advocacia Terra Moreira\"/></md-button><span>Acesse nossa página no Facebook</span></div>");
+$templateCache.put("components/footer/footer.html","<div layout=\"column\" layout-gt-sm=\"row\" class=\"content\"><div layout=\"column\" layout-gt-xs=\"row\" flex=\"50\" layout-align=\"center center\"><contact-info icon=\"phone\" phone1=\"2523-8533\" phone2=\"3018-6128\" phone3=\"3515-5099\"></contact-info><contact-info icon=\"smartphone\" phone1=\"98563-2664\" phone2=\"99376-3195\" phone3=\"98104-7997\"></contact-info></div><div layout-align=\"center center\" layout=\"column\" flex=\"50\"><span class=\"footer-text md-title\">Rua Tenente Brito de Melo, 404</span><span class=\"footer-text md-title\">Barro Preto - Belo Horizonte/MG</span><span class=\"footer-text md-title\">CEP: 30180-072</span></div></div>");
+$templateCache.put("components/toolbar/toolbar.html","<md-toolbar md-scroll-shrink=\"\" class=\"md-toolbar md-whiteframe-12dp\"><div layout=\"row\" layout-align=\"space-between center\" class=\"md-toolbar-tools content\"><img ng-src=\"{{$ctrl.image}}\" alt=\"Advocacia Terra Moreira\" ng-click=\"$ctrl.redirectHomePage()\" class=\"md-card-image\"/><md-nav-bar flex=\"\" md-selected-nav-item=\"$ctrl.currentNavItem\" nav-bar-aria-label=\"navigation links\" hide=\"hide\" show-gt-sm=\"\"><md-nav-item ng-repeat=\"item in $ctrl.navbar\" md-nav-sref=\"{{item.state}}\" name=\"{{item.title}}\">{{item.title}}</md-nav-item></md-nav-bar><div hide-gt-sm=\"\" layout=\"row\" flex=\"\" layout-align=\"space-between center\"><span flex=\"flex\" class=\"mobile-title\">ADVOCACIA TERRA MOREIRA</span><md-button><i class=\"inset material-icons\">menu     </i></md-button></div></div><div layout=\"row\" class=\"degrade\"></div></md-toolbar>");}]);
--- server/routes.js
+++ server/routes.js
@@ -5,20 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function (app, config) {
-  // Insert routes below
-  app.use('/auth', require('./auth').default);
-
-  app.route('/login').get(function (req, res) {
-    var errorMessage = req.flash('error');
-
-    if (errorMessage.length > 0) {
-      var cookieName = 'error-login';
-      res.cookie(cookieName, config.I18N.en[config.cookies[cookieName]]);
-    }
-
-    res.sendFile(_path2.default.resolve(app.get('appPath') + '/index.html'));
-  });
-
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(_errors2.default[404]);
 
